d880310ae3130938530ecb3fcc039d7d85c3fcb4
Tanaka Arata <ky.37f.9850@s.thers.ac.jp>
2025-10-10 14:20:58 +0900

    fix: entry point .sh

--- docker/scripts/entrypoint_addition/dds-setting.sh
+++ docker/scripts/entrypoint_addition/dds-setting.sh
@@ -20,22 +20,6 @@ print_info() {
     echo "workspace-entrypoint: $1"
 }
 
-print_info "Creating non-root container '${USER_NAME}' for host user uid=${HOST_USER_UID}:gid=${HOST_USER_GID}"
-
-groupmod -n ${USER_NAME} -g ${HOST_USER_GID} $(getent group ${HOST_USER_GID} | cut -d: -f1) &>/dev/null || \
-groupadd -g ${HOST_USER_GID} ${USER_NAME} &>/dev/null
-
-usermod -l ${USER_NAME} -u ${HOST_USER_UID} -g ${HOST_USER_GID} $(getent passwd ${HOST_USER_UID} | cut -d: -f1) &>/dev/null || \
-useradd -u ${HOST_USER_UID} -g ${HOST_USER_GID} -m -s /bin/bash ${USER_NAME} &>/dev/null
-
-echo "${USER_NAME} ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/${USER_NAME}
-chmod 0440 /etc/sudoers.d/${USER_NAME}
-
-usermod -a -G dialout,sudo,video,audio,input ${USER_NAME}
-id jtop &>/dev/null && usermod -a -G jtop ${USER_NAME}
-
-print_info "Applying custom network settings..."
-
 # 1. multicastを有効化
 /usr/sbin/ip link set lo multicast on
 
